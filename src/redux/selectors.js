f6b6ce9679a985e527b3c9a2b847d353ef42faec
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-06-06 00:28:52 +0300

    selector+operation
    з помилками

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,17 @@
-export const getContacts = state => state.contacts.contacts;
+// export const getContacts = state => state.contacts.contacts;
 
-export const getFilter = state => state.filter;
+// export const getFilter = state => state.filter;
+
+export const selectContacts = state => state.contacts.items;
+export const selectFilter = state => state.filter;
+export const selectError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.isLoading;
+
+export const selectFilteredContacts = state => {
+  const contacts = selectContacts(state);
+  const filter = selectFilter(state);
+
+  return contacts?.filter(contact =>
+    contact?.name?.toLowerCase().includes(filter.toLowerCase())
+  );
+};
